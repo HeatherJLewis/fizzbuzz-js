56725e5e63aeea1c82a7f08fa2c7d2b1becd134f
Heather Lewis <heather.lewis@bbc.co.uk>
2020-06-24 12:03:33 +0100

    Adds condition for multiple of 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,19 +3,34 @@
 // Here, we create our main function.
 function fizzbuzz() {
     // Put your code here..
-    for (let i = 1; i < 11; i++) {
+    for (let i = 1; i < 200; i++) {
         let result = '';
+
         if(i % 3 === 0){
-            result += 'Fizz';
-        } else if (i % 5 === 0)
-        {
-            result += 'Buzz';
-        } else if (i % 7 === 0){
-            result += 'Bang'
-        } else {
-            result = i;
+            result += 'Fizz'
+        };
+
+        if(i % 13 === 0){
+            result += 'Fezz'
+        };
+
+        if(i % 5 === 0){
+            result += 'Buzz'
+        };
+
+        if(i % 7 === 0){
+            result += 'Bang'};
+
+        if(i % 11 === 0){
+            result = (result === 'Fezz') ? result += 'Bong' : 'Bong'
         }
-    console.log(result);
+
+
+        if(result == ''){
+            result = i;
+        };
+
+        console.log(result);
 
     }
 }
