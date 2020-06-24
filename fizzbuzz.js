ca9e69673c87e0846c1313f7c6759ea8a0eaf452
Heather Lewis <heather.lewis@bbc.co.uk>
2020-06-24 11:20:54 +0100

    Adds basic function for part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,22 @@
 
 
 // Here, we create our main function.
-function fizzbuzz() {
-    
-    console.log('Welcome to FizzBuzz!');
+function fizzbuzz(number) {
+    // Put your code here..
+    for (let i = 0; i < number; i++) {
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizz buzz');
+        } else if (i % 5 === 0)
+        {
+            console.log('buzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
 
-    // Put your code here...
-
-    
+        } else {
+            console.log(i)
+        }
+    }
 }
 
 // Now we run the main function...
-fizzbuzz();
+fizzbuzz(10); // if we add a breakpoint here and use step into it will go into function
